57525b7c0fcbb6190da9556e39f46b7e2514d770
Arnold Tan <tanatarnold@gmail.com>
2015-10-19 10:56:38 +0800

    prepared for CEP final project

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var cookieParser = require('cookie-parser');
 var path = require('path');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
-var toobusy = require('toobusy');
-toobusy.maxLag(10);
 
 var TeacherController = require('./lib/TeacherController.js');
 var StudentController = require('./lib/StudentController.js');
@@ -26,14 +24,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function(req, res, next) {
-  if (toobusy()) {
-    res.status(503).send("I'm busy right now, sorry.");
-  } else {
-    next();
-  } 
-});
 
 app.get('/', function (req, res) {
     if (req.session.authenticated) return res.redirect('/app');
